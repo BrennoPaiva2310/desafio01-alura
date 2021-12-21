6c48fd70342c6ae9d6a42a38a5df0d0ad31c1b7c
Brenno de Paiva <brennopaivag@gmail.com>
2021-12-21 10:28:54 -0300

    Primeiro desafio PHP da alura

---
+++ desafio01.php
@@ -0,0 +1,15 @@
+<?php
+
+for($i = 0; $i <=100;$i++){
+    if($i % 2 != 1){
+        echo $i.PHP_EOL;
+    }
+}
+
+$i = 0;
+while($i <=100){
+    if($i % 2 != 1){
+        echo $i.PHP_EOL;
+    }
+    $i++;
+}
